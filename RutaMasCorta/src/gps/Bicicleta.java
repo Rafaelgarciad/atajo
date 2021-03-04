19fa29c39aabc3170dc001e79522d797f289ab89
garri <garri@10.0.2.15>
2021-03-04 03:33:35 +0100

    Realizada la correcion

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -2,55 +2,79 @@ package gps;
 /**
  * Bicicleta class
  * 
- * @Java IES Severo Ochoa
- * @2.0
+ * @author Rafael Garcia Diaz
+ * @version
  */
 
 public class Bicicleta {
+	
+	/**
+	 * clase Bicicleta
+	 */
     public Bicicleta(){
         int ruedas = 4;
         boolean luces = false;
         String marca = "Orbea";
     }
     
+    
+    /**
+     * 
+     * @param camino1
+     * @param camino2
+     * @param camino3
+     * @return devuelve el camino mas corto
+     */
     public static String rutaCorta(int [] camino1, int [] camino2, int [] camino3)
     {
         int sum1 = 0;
         int sum2 = 0;
         int sum3 = 0;
         
-        for(int i=0; i<camino1.length; i++)
-        {
+        // separamos los arrays para que se recorran de manera separada y asi
+        //poder realizar el calculo correcto
+        for(int i=0; i<camino1.length; i++){
             sum1 += camino1[i];
-            sum2 += camino2[i];
-            sum3 += camino3[i];
         }
         
-        if(sum1 < sum2 && sum1 < sum3)
-        {
+        for(int i=0; i<camino2.length; i++) {
+        	sum2 += camino2[i];
+        }
+        
+        for(int i=0; i<camino3.length; i++) {
+        	sum3 += camino3[i];
+        }
+        
+        if(sum1 < sum2 && sum1 < sum3){
             return "camino 1"; 
-        }else if(sum2 < sum3 && sum2 < sum1)
-        {
+            
+        }else if(sum2 < sum3 && sum2 < sum1){
             return "camino 2";
-        }else if(sum3 < sum1 && sum3 < sum2)
-        {
+            
+        }else if(sum3 < sum1 && sum3 < sum2){
             return "camino 3";
+            
         }else{
-            if(sum1 == sum2 && sum2 == sum3)
-            {
+        	
+            if(sum1 == sum2 && sum2 == sum3){
                 return "camino 1, 2 y 3";
-            }else if(sum1 == sum3)
-            {
+                
+            }else if(sum1 == sum3){
                 return "caminos 1 y 3";
+                
             }else if(sum2 == sum3){
                 return "caminos 2 y 3";
+                
             }else{
                 return "caminos 1 y 2";
             }
         }
-        
     }
-
+    
+    /**
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         Bicicleta bici = new Bicicleta();
         int[] camino1 = {12,34,21,46,25};
